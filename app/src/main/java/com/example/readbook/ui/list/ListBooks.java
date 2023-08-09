826352e2d3c80e0579ce876e26344eb4230ecd7d
NguyenHuyHoai <huyhoai1712@gmail.com>
2023-08-09 10:38:16 +0700

    Update

---
+++ app/src/main/java/com/example/readbook/ui/list/ListBooks.java
@@ -0,0 +1,16 @@
+package com.example.readbook.ui.list;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.readbook.R;
+
+public class ListBooks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_books);
+    }
+}
